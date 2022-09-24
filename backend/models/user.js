231403b125eb1f33a6d14be26c8862b43b7708ba
Clement Jeulin <clement93jeulin@gmail.com>
2022-09-24 10:36:06 +0200

    Ajout de mongoose-unique-validator OK (permet une vérification renforcée de l'unicité de l'adresse mail)

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,16 @@
-// // IMPORT OF : Mongoose
+//IMPORT OF : Mongoose
 const mongoose = require('mongoose')
 
-// Data Model for the signup 
+//IMPORT OF : mongoose unique validator
+const uniqueValidator = require('mongoose-unique-validator')
 
+// DataBase Model for the user's signup 
 const userSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 })
 
+//Pour empêcher l'enregistrement de 2 adresses mail similaires dans la database 
+userSchema.plugin(uniqueValidator)
 //Module exportation 
 module.exports = mongoose.model("user", userSchema)
