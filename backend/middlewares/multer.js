0c1dd17e4f70e9eca908f032ecc8a5d083bf2464
Clement Jeulin <clement93jeulin@gmail.com>
2022-10-12 10:22:42 +0200

    Création du fichier multer.js

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,34 @@
+//MULTER : Pour gérer les requête HTTP avec envoie de fichier 
+
+//IMPORT OF : Multer
+const multer = require ('multer')
+
+//Le dictionnaire de MIME_TYPES (cela sert à définir les formats de fichiers qui seront acceptés)
+const MIME_TYPES = {
+    "image/jpg" : "jpg",
+    "image/jpeg" : "jpeg",
+    "image/gif" : "gif",
+    "image/png" : "jpg"
+}
+
+console.log(MIME_TYPES)
+
+//La destination du fichier (répertoire) et génerer un nom de fichier unique 
+const storage = multer.diskStorage({
+    //La destination de stockage du fichier
+     destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    filename: (req, file, callback) => {
+        //suppression des espaces pour une meilleur lisibilité du nom de fichier
+        const name = file.originalname.split(" ").join("_")
+        const extension = MIME_TYPES[file.mimetype];
+
+        callback(null, name + "_" + Date.now() + extension);
+    }
+}
+)
+
+//Exportation 
+module.exports = multer({storage}).single('image')
+
