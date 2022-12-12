5fc6a73749aae546887416740e4e09b780e396af
Clement Jeulin <clement93jeulin@gmail.com>
2022-12-12 11:38:12 +0100

    Retrait console.log

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,11 +6,9 @@ const router = express.Router();
 
 //IMPORT DE : password model
 const password = require('../middlewares/password')
+
 //Import du controller user.js 
 const userController = require('../controllers/user')
-console.log("CONTENU DE : userController")
-console.log(userController)
-
 
 //La route (endpoint) signup
 router.post("/signup", password, userController.signup)
